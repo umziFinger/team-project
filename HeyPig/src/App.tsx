803cf27a106691231dff31958b8023aa01c1768e
KJ Mac <kjmac@DTui-MacBookPro.local>
2021-11-02 13:47:20 +0900

    Day2_skeleton

--- HeyPig/src/App.tsx
+++ HeyPig/src/App.tsx
@@ -1,7 +1,10 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Text, TextInput, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {MyPageScreen} from './components/unit/diary'
+import { BoardScreen } from './components/unit/board';
+import { MarketScreen } from './components/unit/market';
 
 function HomeScreen() {
   return (
@@ -11,22 +14,6 @@ function HomeScreen() {
   );
 }
 
-function SettingsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
-    </View>
-  );
-}
-
-function MyPageScreen() {
-  return(
-    <View style={{flex:1, justifyContent:'center',alignItems:'center'}}>
-      <Text>MyPage!</Text>
-    </View>
-  )
-}
-
 const Tab = createBottomTabNavigator();
 
 export default function App() {
@@ -34,8 +21,9 @@ export default function App() {
     <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
-        <Tab.Screen name="MyPage" component={MyPageScreen} />
+        <Tab.Screen name="Board" component={BoardScreen} />
+        <Tab.Screen name="Market" component={MarketScreen} />
+        <Tab.Screen name="Diary" component={MyPageScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
---
+++ HeyPig/src/components/unit/diary/index.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { Image, Text, TextInput, View, ScrollView, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    DiaryView: {
+        height: 100, 
+        width: 350, 
+        borderRadius:10, 
+        backgroundColor: "white", 
+        margin: 10, 
+        justifyContent:'center', 
+        paddingHorizontal: 30,
+        elevation: 3,
+    },
+
+    DiaryImage: {
+        justifyContent:'center', 
+        alignItems:'center'
+    }
+})
+
+export function MyPageScreen() {
+
+    return(
+      <View style={{flex:1, justifyContent:'center',alignItems:'center'}}>    
+        <ScrollView>
+            <View>
+                {new Array(10).fill(1).map((el,i) => 
+                <View key={i} style={styles.DiaryView}>
+                    <Image style={styles.DiaryImage} source={require('../../../Assets/images/add.png')} />
+                </View>)}
+            </View>
+        </ScrollView>
+      </View>
+    )
+  }
