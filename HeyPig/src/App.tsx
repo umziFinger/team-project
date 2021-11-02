a26cb5d04bee69b0eb5d8d5a00af6822a8ecca9d
오수향 <suhyang@osuhyangs-MacBook-Pro.local>
2021-11-02 18:06:34 +0900

    temp

--- HeyPig/src/App.tsx
+++ HeyPig/src/App.tsx
@@ -1,30 +1,65 @@
 import * as React from 'react';
-import { Text, TextInput, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { MyPageScreen } from './components/unit/diary'
-import { BoardScreen } from './components/unit/board';
-import { MarketScreen } from './components/unit/market';
+import {Text, TextInput, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {MyPageScreen} from './components/unit/diary';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
+import {DiaryDetailScreen} from './components/unit/diaryDetail';
+import {BoardMain} from './components/unit/board/boardMain';
+import {BoardWrite} from './components/unit/board/boardWrite';
+import {BoardDetail} from './components/unit/board/boardDetail';
+import {MarketMain} from './components/unit/market/marketMain';
+import {MarketWrite} from './components/unit/market/marketWrite';
+import {MarketDetail} from './components/unit/market/marketDetail';
 
 function HomeScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>Home!</Text>
     </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+function Board() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="BoardMain" component={BoardMain} />
+      <Stack.Screen name="BoardWrite" component={BoardWrite} />
+      <Stack.Screen name="BoardDetail" component={BoardDetail} />
+    </Stack.Navigator>
+  );
+}
+
+function Market() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="MarketMain" component={MarketMain} />
+      <Stack.Screen name="MarketWrite" component={MarketWrite} />
+      <Stack.Screen name="MarketDetail" component={MarketDetail} />
+    </Stack.Navigator>
   );
 }
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
+  const client = new ApolloClient({
+    uri: 'http://backend03.codebootcamp.co.kr/graphql',
+    cache: new InMemoryCache(),
+  });
+
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Board" component={BoardScreen} />
-        <Tab.Screen name="Market" component={MarketScreen} />
-        <Tab.Screen name="Diary" component={MyPageScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <ApolloProvider client={client}>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Board" component={Board} />
+          <Tab.Screen name="Market" component={Market} />
+          <Tab.Screen name="Diary" component={MyPageScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </ApolloProvider>
   );
 }
--- HeyPig/src/components/unit/board/index.tsx
+++ HeyPig/src/components/unit/board/index.tsx
@@ -1,36 +1,66 @@
 import * as React from 'react';
-import { Image, Text, TextInput, View, ScrollView, StyleSheet } from 'react-native';
+import {
+  Image,
+  Text,
+  TextInput,
+  View,
+  ScrollView,
+  StyleSheet,
+  Button,
+} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 const styles = StyleSheet.create({
-    DiaryView: {
-        height: 100, 
-        width: 350, 
-        borderRadius:10, 
-        backgroundColor: "white", 
-        margin: 10, 
-        justifyContent:'center', 
-        paddingHorizontal: 30,
-        elevation: 3,
-    },
+  BoardView: {
+    height: 50,
+    width: 380,
+    // borderRadius: 10,
+    backgroundColor: 'white',
+    margin: 5,
+    justifyContent: 'center',
+    paddingHorizontal: 10,
+    // elevation: 3,
+  },
 
-    DiaryImage: {
-        justifyContent:'center', 
-        alignItems:'center'
-    }
-})
+  DiaryImage: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
-export function BoardScreen() {
-
-    return(
-      <View style={{flex:1, justifyContent:'center',alignItems:'center'}}>    
-        <ScrollView>
-            <View>
-                {new Array(10).fill(1).map((el,i) => 
-                <View key={i} style={styles.DiaryView}>
-                    <Image style={styles.DiaryImage} source={require('../../../Assets/images/add.png')} />
-                </View>)}
-            </View>
-        </ScrollView>
+export function BoardMain({navigation}) {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+        <Text>익명게시판</Text>
+        <Text> | </Text>
+        <Text>자랑게시판</Text>
       </View>
-    )
-  }
+      <ScrollView>
+        <View>
+          <View style={styles.BoardView}>
+            <Text>배고파</Text>
+          </View>
+          <View style={styles.BoardView}>
+            <Text>뭐먹지</Text>
+          </View>
+
+          {/* {new Array(5).fill(1).map((el, i) => (
+            <View key={i} style={styles.BoardView}></View>
+          ))} */}
+        </View>
+      </ScrollView>
+      <Button title="작성" onPress={() => navigation.navigate('BoardWrite')} />
+    </View>
+  );
+}
+
+// function BoardWrite() {
+//   return (
+//     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+//       <Text>작성페이지</Text>
+//     </View>
+//   );
+// }
+// const Stack = createNativeStackNavigator();
