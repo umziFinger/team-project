8ab8a906eb2263aff43a5ef4a4932e7d7529c15f
umziFinger <chlrnjswns1004@gmail.com>
2021-11-08 17:08:54 +0900

    temp

--- HeyPig/src/components/unit/login/index.tsx
+++ HeyPig/src/components/unit/login/index.tsx
@@ -9,9 +9,9 @@ import {
   TextInput,
   View,
 } from 'react-native';
-import AsyncStorage from '@react-native-community/async-storage';
 
 import {gql, useMutation} from '@apollo/client';
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 export const LOGIN_USER = gql`
   mutation loginUser($email: String!, $password: String!) {
