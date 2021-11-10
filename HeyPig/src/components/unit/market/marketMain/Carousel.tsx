c71b9f410a88699e98aca6df62fb7535b963d94b
qqwwweeeer <dudqja3674@gamail.com>
2021-11-10 14:22:59 +0900

    Day8_market_S

--- HeyPig/src/components/unit/market/marketMain/Carousel.tsx
+++ HeyPig/src/components/unit/market/marketMain/Carousel.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import {FlatList} from 'react-native';
-import styled from 'styled-components/native';
+import styled from 'styled-components/native';  
 import Page from './Page';
 
 interface ICarousel {
